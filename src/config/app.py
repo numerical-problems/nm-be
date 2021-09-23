34c56e966c513533184aa086ffee65be69f4b56c
vineboneto <vineboneto@gmail.com>
2021-09-23 20:48:00 -0300

    refactor: adicionado CORS globalmente

--- src/config/app.py
+++ src/config/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from .config import config_by_name
 from .api_rest import blueprint
 
 
+
 def create_app() -> Flask:
     app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "*"}})
     app.config.from_object(config_by_name['dev'])
     app.register_blueprint(blueprint)
     return app
