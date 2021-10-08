8ee7d765ef647c8343b9ffea70280f75777733f0
Icaro Peretti <icaroperetti50@gmail.com>
2021-10-08 13:07:10 -0300

    refactor: Deixando o parâmetro de quantidades de vezes como opcional no body

--- src/controllers/derivate_controller.py
+++ src/controllers/derivate_controller.py
@@ -9,9 +9,7 @@ class DerivativeController(Http):
     # Derivada
     def derivate_expression(self, body):
         related_to = body['related_to']
-        times = 1
-        if(body['times']):
-            times = body['times']
+        times = body['times'] if 'times' in body else 1
         if related_to == 'x' or related_to == 'y' or related_to == 'z':
             try:
                 result = sym.diff(body['expression'], related_to, times)
