ac7b6a0d5021181d0f346fac089591bff6d7c55d
Icaro Peretti <icaroperetti50@gmail.com>
2021-10-22 11:26:32 -0300

    refactor: Alterando sympy diff para Derivative

--- src/controllers/derivate_controller.py
+++ src/controllers/derivate_controller.py
@@ -1,19 +1,21 @@
 from .http import Http
 import sympy as sym
-from sympy import *
+from sympy.abc import x, y, z
+from sympy import Derivative
 
 
 class DerivativeController(Http):
-    x, y, z = sym.symbols('x y z')
 
     # Derivada
     def derivate_expression(self, body):
+        expr = body['expression']
         related_to = body['related_to']
         times = body['times'] if 'times' in body else 1
         if related_to == 'x' or related_to == 'y' or related_to == 'z':
             try:
-                result = sym.diff(body['expression'], related_to, times)
-                return self._return_result(result)
+                result = Derivative(expr)
+                return self._return_result(result.doit())
+
             except Exception as e:
                 return self.server_error()
         else:
