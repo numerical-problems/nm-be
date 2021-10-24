7c4203199bb04f13d359400d935bb3603ec6fbb6
vineboneto <vineboneto@gmail.com>
2021-10-23 21:42:36 -0300

    test: ensure 200 on calcule serie fourier

--- src/controllers/fourier_serie_controller.py
+++ src/controllers/fourier_serie_controller.py
@@ -13,15 +13,10 @@ class FourierSerieController(Http):
             second_interval = body["second_interval"]
             if not "x" in expression:
                 return self.bad_request(message='Missing "x" in expression')
-            serie = fourier_series(expression, (x, int(first_interval), int(second_interval)))
+            if float(first_interval) == float(second_interval):
+                return self.bad_request(message="Intervals cannot be equal")
+            serie = fourier_series(expression, (x, float(first_interval), float(second_interval)))
             serie_result = serie.truncate(int(n))
-            return self.__check_nan(serie_result)
+            return self.ok(dict(result=str(serie_result)))
         except Exception as e:
             return self.server_error(e)
-
-    def __check_nan(self, serie_result):
-        return (
-            self.ok(dict(result="Invalid expression"))
-            if serie_result == nan
-            else self.ok(dict(result=str(serie_result)))
-        )
---
+++ tests/test_fourier.py
@@ -0,0 +1,11 @@
+from tests.test_config import client
+
+
+def test_fourier_200(client):
+    body = dict(first_interval=-3.14, second_interval=3.14, expression="x + 1", n=3)
+    response = client.post("/fourier", json=body)
+    data = response.json
+    assert data == dict(
+        result="6.28*sin(0.318471337579618*pi*x)/pi - 3.14*sin(0.636942675159236*pi*x)/pi + 1.0"
+    )
+    assert response.status_code == 200
