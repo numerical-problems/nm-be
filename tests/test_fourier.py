c7ef85fa8f638facbfaa8189fb3ce422f0271afb
vineboneto <vineboneto@gmail.com>
2021-10-23 21:44:32 -0300

    test: ensure 400 on calcule serie fourier

--- tests/test_fourier.py
+++ tests/test_fourier.py
@@ -9,3 +9,15 @@ def test_fourier_200(client):
         result="6.28*sin(0.318471337579618*pi*x)/pi - 3.14*sin(0.636942675159236*pi*x)/pi + 1.0"
     )
     assert response.status_code == 200
+
+
+def test_fourier_400_missing_x(client):
+    body = dict(first_interval=-3.14, second_interval=3.14, expression="y + 1", n=3)
+    response = client.post("/fourier", json=body)
+    assert response.status_code == 400
+
+
+def test_fourier_400_invalid_interval(client):
+    body = dict(first_interval=3.14, second_interval=3.14, expression="x + y + 1", n=3)
+    response = client.post("/fourier", json=body)
+    assert response.status_code == 400
