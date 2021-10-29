fddb94ddc1084ce450efec5a6bbb28605999e83f
Icaro Peretti <icaroperetti50@gmail.com>
2021-10-29 10:58:43 -0300

    feat:Adicionando teste para integrais com limites

--- tests/test_integrals.py
+++ tests/test_integrals.py
@@ -6,3 +6,10 @@ def test_integrals_query(client):
     response = client.post("/integrals", json=body)
     data = response.json
     assert data == (dict(result="-log(sin(x) - 1)/2 + log(sin(x) + 1)/2 - sin(x)"))
+
+
+def test_integrals_limit(client):
+    body = dict(expression="2*x**2+1", related_to="x", superior_limit=2, inferior_limit=1)
+    response = client.post("/integrals/limits", json=body)
+    data = response.json
+    assert data == (dict(result="17/3"))
