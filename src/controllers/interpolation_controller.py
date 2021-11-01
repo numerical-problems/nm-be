58231c0fe3db838ee9b0d592ffdbf0918a3f7c51
Rafael Bortolozo <rafaelbortolozo@outlook.com>
2021-11-01 17:44:22 -0300

    convert string to array

--- src/controllers/interpolation_controller.py
+++ src/controllers/interpolation_controller.py
@@ -3,18 +3,27 @@ import numpy as np
 from sympy import *
 
 class InterpolationController(Http):
-    def calculation(self, body):
-      coordinates = body["coordinates"]
-      return self._return_result(coordinates)
+    def stringToArray(self, string): #ex: 1,2;3,4 -> [[1.0, 2.0], [3.0, 4.0]]
+      string = string.split(";")
+      array = []
+      aux = []
+      for i in string:
+        coordenada = i.split(",") 
+        aux = []
+        for j in coordenada:
+          aux.append(float(j))
+        array.append(aux)
+      return array      
 
-    # def interpolation_expression(self, body):
-    #     try:
-    #         expression = return calculation()
-    #         return self._return_result(expression)
-    #     except Exception as e:
-    #         if str(e).find("Sympify of expression 'could not parse") != -1:
-    #             return self.bad_request({"expressionError": "The expression is not valid"})
-    #         return self.server_error()
+    def interpolation_expression(self, body):
+        try:
+            coordinates = body["coordinates"]
+            array = self.stringToArray(coordinates) 
+            return self._return_result(array) 
+        except Exception as e:
+            if str(e).find("Sympify of expression 'could not parse") != -1:
+                return self.bad_request({"expressionError": "The expression is not valid"})
+            return self.server_error()
 
     def _return_result(self, result):
         return self.ok({"result": str(result)})
