6c7eb7e294ece4f630bcedc71138784d30bd46aa
tiago-ferrari <69017840+tiago-ferrari@users.noreply.github.com>
2021-11-19 10:19:43 -0300

    Update routes.py

--- src/config/routes.py
+++ src/config/routes.py
@@ -4,6 +4,7 @@ from src.routes.derivate_routes import api as derivate_api
 from src.routes.fourier_routes import api as fourier_api
 from src.routes.integrate_routes import api as integrate_api
 from src.routes.interpolation_routes import api as interpolation_api
+from src.routes.bissection_routes import api as bissection_api
 
 
 def set_routes(api: Api) -> Api:
@@ -14,4 +15,5 @@ def set_routes(api: Api) -> Api:
     api.add_namespace(fourier_api, path="/fourier")
     api.add_namespace(integrate_api, path="/integrals")
     api.add_namespace(interpolation_api, path="/interpolation")
+    api.add_namespace(bissection_api,path='/bissection')
     return api
